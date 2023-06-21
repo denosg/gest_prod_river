4089132647e1b2725fb2e9486883c4ff2d89c11c
denos <costelasdenis@gmail.com>
2023-06-21 19:18:50 +0300

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   //Makes it so SystemChrome.setPreferredOrientations works
@@ -7,7 +8,7 @@ void main() {
   //Sets preffered orientations
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   //Runs the app on boot
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
