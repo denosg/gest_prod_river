19ad07bc68a60e8cf27b5a8d93d6110ca8324932
denos <costelasdenis@gmail.com>
2023-06-28 14:17:25 +0300

    modified addListingInList to add to cloud db

--- lib/providers/listing_list_provider.dart
+++ lib/providers/listing_list_provider.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gest_prod_river/models/listing.dart';
 
@@ -8,10 +9,49 @@ final listingListProvider =
 class ListingListNotifier extends StateNotifier<List<Listing>> {
   ListingListNotifier() : super([]);
 
-  void addListingInList(Listing currentListing) {
-    print('Listing added in list.');
-    final updatedList = [...state];
-    updatedList.insert(0, currentListing);
-    state = updatedList;
+  void addListingInList(Listing currentListing) async {
+    DatabaseReference databaseRef =
+        FirebaseDatabase.instance.ref().child('listings');
+
+    DatabaseReference newListingRef = databaseRef.push();
+    String itemId = newListingRef.key!;
+
+    Map<String, dynamic> newListingData = {
+      'id': itemId,
+      'title': currentListing.title,
+      'dateTime': currentListing.dateTime.toIso8601String(),
+      'amount': currentListing.amount,
+      'items': currentListing.itemList
+          .map((item) => {
+                'title': item.title,
+                'photoUrl': item.photoUrl,
+                'pricePaid': item.pricePaid,
+                'priceMarket': item.priceMarket,
+                'amount': item.amountOfItem,
+              })
+          .toList(),
+    };
+
+    try {
+      await newListingRef.set(newListingData);
+      print('Listing added to Firebase successfully!');
+
+      // Update the state object of currentListing
+      Listing updatedListing = Listing(
+        id: itemId, // Update the ID with the newly generated key
+        title: currentListing.title,
+        dateTime: currentListing.dateTime,
+        amount: currentListing.amount,
+        itemList: currentListing.itemList,
+      );
+      //adding the listing locally in the memory
+      final updatedList = [...state];
+      updatedList.insert(0, updatedListing);
+      state = updatedList;
+
+      print('Listing added in list with updated state object.');
+    } catch (error) {
+      print('Failed to add listing to Firebase: $error');
+    }
   }
 }
